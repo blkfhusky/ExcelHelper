dfe8b12d8852d6a2b8cbdd27f179ae4a9da55f12
BlkFHusky <blkfhusky@BlkFHuskydeMacBook-Pro.local>
2018-01-27 17:33:11 +0800

    Initial commit

---
+++ src/main/java/com/zxc/Application.java
@@ -0,0 +1,11 @@
+package com.zxc;
+
+/**
+ * @author BlkFHusky
+ *
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
